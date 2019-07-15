d6c2168c4c1b707a2122e89fd33e6a493fdfc3d7
towa <981135072@qq.com>
2019-07-15 16:35:13 +0800

    admin 2019/7/15

---
+++ demo/src/main/java/blog/demo/controller/admin/BlogController.java
@@ -0,0 +1,12 @@
+package blog.demo.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author towa
+ */
+@Controller
+@RequestMapping("/admin")
+public class BlogController {
+}
